48bd8015ebbd85a03b1010a8977f6a0a8463713a
ASHUTOSH BISWAL <sharewithme2002@gmail.com>
2023-02-21 00:24:55 +0530

    Added a new project

---
+++ footer.php
@@ -0,0 +1,4 @@
+<div id="container">
+    <div id="header"> Made by Ashutosh Biswal © 2023. <br>
+      For the Project of The Sparks Foundation</div>
+ </div>
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include('nav.php') ?>
+
+  <html>
+    <head>
+      
+    </head>
+  <h1>Welcome to DEMO bank</h1>
+
+
+  <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class ="bg bg3"></div>
+    <div class="center">
+      <ul>
+        <div class="btn7"><li class="operations"><a href="transfer_money.php"><button class="btn" id="red"><b>VIEW ALL THE USERS</b></button></a></li></div>
+        <br>
+        <div class="btn7"><li class="operations"><a href="transfer_money.php"><button class="btn" id="blue"><b>TRANSFER MONEY</b></button></a></li></div>
+        <br>
+        <div class="btn7"><li class="operations"><a href="transactionhistory.php"><button class="btn" id="green"><b>TRANSFER HISTORY</b></button></a></li></div>
+      </ul>
+    </div>
+    </html>
+  
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
